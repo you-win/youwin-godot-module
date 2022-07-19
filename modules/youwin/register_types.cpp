30745a6351e8d973348beaff0ae766a2897ded9a
Tim Yuen <timothy.yuen22@gmail.com>
2022-07-19 14:53:36 -0400

    move singleton registration to register_types, no longer needs a patch

--- modules/youwin/register_types.cpp
+++ modules/youwin/register_types.cpp
@@ -1,12 +1,23 @@
 #include "register_types.h"
 
+#include "core/engine.h"
+
 #include "result.h"
+
+static Safely *safely = nullptr;
 
 void register_youwin_types() {
 	ClassDB::register_class<SafeError>();
 	ClassDB::register_class<Result>();
 	ClassDB::register_class<Safely>();
+
+	safely = Safely::create();
+
+	Engine::get_singleton()->add_singleton(Engine::Singleton("Safely", Safely::get_singleton()));
 }
 
 void unregister_youwin_types() {
+	if (safely) {
+		memdelete(safely);
+	}
 }
